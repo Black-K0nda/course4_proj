b9857e13565d437f8db045a4ab8476c0092c39b7
Bartly Dyck <bartly.dyck@hotmail.ca>
2024-04-09 19:22:23 +0000

    Finish scheduling with celery

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -50,6 +50,7 @@ class Dev(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        'django_celery_beat',
         'django_celery_results',
         'movies',
 
